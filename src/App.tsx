4a96effb8adc6c23d66b2fec6184a9ac2ba506af
刘明 <mingliu63@creditease.cn>
2025-01-06 15:08:46 +0800

    feat: layout插件开发

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import LogicFlow from '@logicflow/core';
+import '@logicflow/core/dist/index.css';
+import { Layout } from './packages/layout';
+import { useEffect, useMemo, useRef } from 'react';
+import { Button } from 'antd';
+
+export default function App() {
+  const refContainer = useRef(null);
+  const lfRef = useRef<LogicFlow>();
+  const data = useMemo(() => {
+    return {
+      // 节点
+      nodes: [
+        {
+          id: '21',
+          type: 'rect',
+          x: 300,
+          y: 100,
+          text: 'rect node21',
+        },
+        {
+          id: '50',
+          type: 'circle',
+          x: 500,
+          y: 100,
+          text: 'circle node50',
+        },
+        {
+          id: '51',
+          type: 'circle',
+          x: 400,
+          y: 200,
+          text: 'circle node51',
+        },
+        {
+          id: '52',
+          type: 'circle',
+          x: 200,
+          y: 200,
+          text: 'circle node52',
+        },
+        {
+          id: '53',
+          type: 'circle',
+          x: 200,
+          y: 200,
+          text: 'circle node52',
+        },
+      ],
+      // 边
+      edges: [
+        {
+          type: 'polyline',
+          sourceNodeId: '21',
+          targetNodeId: '50',
+        },
+        {
+          type: 'polyline',
+          sourceNodeId: '21',
+          targetNodeId: '51',
+        },
+        {
+          type: 'polyline',
+          sourceNodeId: '21',
+          targetNodeId: '52',
+        },
+        {
+          type: 'polyline',
+          sourceNodeId: '53',
+          targetNodeId: '21',
+        },
+      ],
+    };
+  }, []);
+  useEffect(() => {
+    const lf = new LogicFlow({
+      container: refContainer.current,
+      grid: true,
+      height: 500,
+      plugins: [Layout],
+    });
+    lf.render(data);
+    lf.translateCenter();
+    lfRef.current = lf;
+  }, [data]);
+
+  return (
+    <>
+      <Button onClick={() => lfRef.current.extension.Layout.layout()}>自动布局</Button>
+      <div className="App" ref={refContainer}></div>
+    </>
+  );
+}
---
+++ src/packages/layout/index.ts
@@ -0,0 +1,249 @@
+import LogicFlow from '@logicflow/core';
+import { AntVDagreLayout, type AntVDagreLayoutOptions } from '@antv/layout';
+import { Graph } from '@antv/graphlib';
+
+export class Layout {
+  lf: LogicFlow;
+  options: AntVDagreLayoutOptions;
+  static pluginName = 'Layout';
+  constructor({ lf }) {
+    this.lf = lf;
+  }
+
+  getBytesLength(word: string): number {
+    if (!word) {
+      return 0;
+    }
+    let totalLength = 0;
+    for (let i = 0; i < word.length; i++) {
+      const c = word.charCodeAt(i);
+      if (word.match(/[A-Z]/)) {
+        totalLength += 1.5;
+      } else if ((c >= 0x0001 && c <= 0x007e) || (c >= 0xff60 && c <= 0xff9f)) {
+        totalLength += 1;
+      } else {
+        totalLength += 2;
+      }
+    }
+    return totalLength;
+  }
+  async layout(options: AntVDagreLayoutOptions = {}) {
+    const initOptions = {
+      nodeSize: 100,
+      begin: [100, 100],
+      align: 'DR',
+      radial: true,
+      rankdir: 'LR',
+    };
+    this.options = Object.assign(initOptions, options);
+    const { nodes, edges } = this.lf.graphModel;
+    const layoutInstance = new AntVDagreLayout(this.options);
+    const graph = new Graph({
+      nodes: nodes.map((node) => ({
+        id: node.id,
+        data: {},
+      })),
+      edges: edges.map((edge) => ({
+        id: edge.id,
+        source: edge.sourceNodeId,
+        target: edge.targetNodeId,
+        data: {},
+      })),
+    });
+    const layoutData = await layoutInstance.execute(graph);
+    const newGraphData = {
+      nodes: [],
+      edges: [],
+    };
+    layoutData.nodes.forEach((node) => {
+      const data = node.data;
+      const originData = this.lf.getNodeModelById(String(node.id)).getData();
+      newGraphData.nodes.push({
+        ...originData,
+        x: data.x,
+        y: data.y,
+        text: {
+          ...originData.text,
+          x: data.x,
+          y: data.y,
+        },
+      });
+    });
+    layoutData.edges.forEach((edge) => {
+      const model = this.lf.getEdgeModelById(String(edge.id));
+      const data = model.getData();
+      data.pointsList = this.calcPointsList(model, layoutData.nodes);
+      if (data.pointsList) {
+        const first = data.pointsList[0];
+        const last = data.pointsList[data.pointsList.length - 1];
+        data.startPoint = { x: first.x, y: first.y };
+        data.endPoint = { x: last.x, y: last.y };
+        if (data.text && data.text.value) {
+          data.text = {
+            x: last.x - this.getBytesLength(data.text.value) * 6 - 10,
+            y: last.y,
+            value: data.text.value,
+          };
+        }
+      } else {
+        // data.startPoint = undefined;
+        // data.endPoint = undefined;
+        // if (data.text && data.text.value) {
+        //   data.text = data.text.value;
+        // }
+      }
+      newGraphData.edges.push(data);
+    });
+    this.lf.render({
+      nodes: layoutData.nodes.map((node) => {
+        const data = node.data;
+        const originData = this.lf.getNodeModelById(String(node.id)).getData();
+        return {
+          ...originData,
+          x: data.x,
+          y: data.y,
+          text: {
+            ...originData.text,
+            x: data.x,
+            y: data.y,
+          },
+        };
+      }),
+      edges: layoutData.edges.map((edge) => {
+        const model = this.lf.getEdgeModelById(String(edge.id));
+        const data = model.getData();
+        data.pointsList = this.calcPointsList(model, layoutData.nodes);
+        if (data.pointsList) {
+          const first = data.pointsList[0];
+          const last = data.pointsList[data.pointsList.length - 1];
+          data.startPoint = { x: first.x, y: first.y };
+          data.endPoint = { x: last.x, y: last.y };
+          if (data.text && data.text.value) {
+            data.text = {
+              x: last.x - this.getBytesLength(data.text.value) * 6 - 10,
+              y: last.y,
+              value: data.text.value,
+            };
+          }
+        } else {
+          // data.startPoint = undefined;
+          // data.endPoint = undefined;
+          // if (data.text && data.text.value) {
+          //   data.text = data.text.value;
+          // }
+        }
+        return data;
+      }),
+    });
+  }
+
+  calcPointsList(model, nodes) {
+    const pointsList = [];
+    const sourceNodeModel = this.lf.getNodeModelById(model.sourceNodeId);
+    const targetNodeModel = this.lf.getNodeModelById(model.targetNodeId);
+    const newSourceNodeData = nodes.find((node) => node.id === model.sourceNodeId).data;
+    const newTargetNodeData = nodes.find((node) => node.id === model.targetNodeId).data;
+    if (this.options.rankdir === 'LR') {
+      // 从左到右
+      if (newSourceNodeData.y === newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x + sourceNodeModel.width / 2,
+          y: newSourceNodeData.y,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x - targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+      if (newSourceNodeData.y < newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newSourceNodeData.y + sourceNodeModel.height / 2,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x - targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+      if (newSourceNodeData.y > newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newSourceNodeData.y - sourceNodeModel.height / 2,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x - targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+    }
+    if (this.options.rankdir === 'RL') {
+      // 从右到左
+      if (newSourceNodeData.y === newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x - sourceNodeModel.width / 2,
+          y: newSourceNodeData.y,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x + targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+      if (newSourceNodeData.y < newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newSourceNodeData.y + sourceNodeModel.height / 2,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x + targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+      if (newSourceNodeData.y > newTargetNodeData.y) {
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newSourceNodeData.y - sourceNodeModel.height / 2,
+        });
+        pointsList.push({
+          x: newSourceNodeData.x,
+          y: newTargetNodeData.y,
+        });
+        pointsList.push({
+          x: newTargetNodeData.x + targetNodeModel.width / 2,
+          y: newTargetNodeData.y,
+        });
+        return pointsList;
+      }
+    }
+    // 从上到下
+    // 从左到右
+  }
+
+  destroy() {
+    // do anything
+  }
+}
